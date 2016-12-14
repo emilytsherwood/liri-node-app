0365d087f499d9652df874cef42ed698aae177a5
Emily Turner-Sherwood <emilyturnerr@gmail.com>
2016-12-14 07:49:18 -0600

    Getting Twitter data back

--- liri.js
+++ liri.js
@@ -1,12 +1,46 @@
 var keyFile = require('./keys.js'); //grabbing data from keys.js file
 
-var nodeArgs = process.argv; //takes in command line arguments
+var Twitter = require('twitter'); 
 
-var twitter = require('twitter'); 
+var tweets = ''; //empty string to hold the tweets
 
-var tweets = ''; //empty strint to hold the tweets
+//Grabbing the API keys from the keys.js file
+var client = new Twitter(keyFile.twitterKeys);
+
+// function myTweets(){
+
+	var params = {screen_name: 'emilyturner88'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+          if(error) {
+            console.log(error);
+          } else {
+          	console.log(tweets);
+          }
+
+            // console.log(tweets); 
+            // console.log(response);   
+});
+
+		  	// for (var i = 0; i < tweets.length; i++) {
+		  	// 	console.log(tweets[i]);
+		  	// }
+		  	// console.log(error);
+		  
+		 
 
 
+		  	
+			// for (var i = 0; i < tweets.length; i++) {
+		 //  	console.log(tweets[i]);
+// }
+// myTweets();
+
+
+// client.get('favorites/list', function(error, tweets, response) {
+//   if(error) throw error;
+//   console.log(tweets);  // The favorites. 
+//   console.log(response);  // Raw response object. 
+// });
 
 // var params = {screen_name: 'nodejs'};
 // client.get('statuses/user_timeline', params, function(error, tweets, response) {
